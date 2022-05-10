ede0b635aadd6ea7437dd8be54ea8b510c8f3e3a
Brenandson <breno.adm15@gmail.com>
2022-05-10 13:01:00 -0300

    feat: configuração e criação do banco de dados

--- ConsumoVeiculos/Models/ApplicationDbContext.cs
+++ ConsumoVeiculos/Models/ApplicationDbContext.cs
@@ -4,9 +4,11 @@ namespace ConsumoVeiculos.Models
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) //configurando a injeção de dependência
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) //configurando a injeção de dependência
         {
-
         }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+
     }
 }
