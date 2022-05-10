02fa2ede609f7fc97fe73ca96200972cf43731ab
Brenandson <breno.adm15@gmail.com>
2022-05-10 12:43:02 -0300

    Configuração e criação do banco de dados.

---
+++ ConsumoVeiculos/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsumoVeiculos.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) //configurando a injeção de dependência
+        {
+
+        }
+    }
+}
